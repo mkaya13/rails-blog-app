0b25445bafbdbe4190dc7b6f61781158ca9a69f7
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-03-08 13:13:49 +0300

    Adds users_index integration tests

---
+++ spec/views/users_index_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :system do
+  describe 'index page' do
+    it 'shows the user name' do
+      visit users_path
+      expect(page).to have_content('Tom')
+    end
+
+    it 'shows the user profile picture' do
+        visit users_path
+        expect(page).to have_selector('img')
+    end
+
+    it 'Can see the number of posts each user has written' do
+        visit users_path
+        expect(page).to have_content('Number of posts:')
+    end
+
+    it 'Can see the number of posts each user has written' do
+        visit users_path
+        expect(page).to have_content('Number of posts:')
+    end
+
+    it 'When I click on a user, I am redirected to that user\'s show page' do
+        visit users_path
+        click_link 'Tom'
+        expect(page).to have_content('Bio')
+    end
+
+  end
+end
